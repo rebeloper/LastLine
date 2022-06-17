c350821f43175ebbede30bccb361561220729a88
rebeloper <therebeloper@gmail.com>
2022-06-17 13:37:20 +0300

    isEditing

--- Sources/LastLine.xcframework/ios-arm64_x86_64-simulator/LastLine.framework/Headers/LastLine-Swift.h
+++ Sources/LastLine.xcframework/ios-arm64_x86_64-simulator/LastLine.framework/Headers/LastLine-Swift.h
@@ -197,6 +197,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import UIKit;
 #endif
 
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
@@ -213,6 +214,13 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="LastLine",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
+
+@class UIEvent;
+
+@interface UIWindow (SWIFT_EXTENSION(LastLine))
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent * _Nullable)event;
+@end
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
@@ -418,6 +426,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import UIKit;
 #endif
 
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
@@ -434,6 +443,13 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="LastLine",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
+
+@class UIEvent;
+
+@interface UIWindow (SWIFT_EXTENSION(LastLine))
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent * _Nullable)event;
+@end
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
